d3c72ef41130c383303727e95ddec6979a24849f
Igor <igorfib6@gmail.com>
2021-04-11 22:58:17 +0200

    (almost) completed solution - only 2 points on SPOJ

--- grant2/zadanie_do_pokazania/mpower_1.cpp
+++ grant2/zadanie_do_pokazania/mpower_1.cpp
@@ -6,6 +6,23 @@ using std::string;
 
 typedef long long ll;
 
+/* string sum(const string &a, const string &b) { */
+/*     /1* cout << "SUM\n"; *1/ */
+/*     while(a.size() > b.size()) */
+/* 	b = '0'+b; */
+/*     while(b.size() > a.size()) */
+/* 	a = '0'+a; */
+/*     int carry = 0; */
+/*     string result; */
+/*     for(int i = a.size()-1; i >= 0; i--) { */
+/* 	int tmp = a[i]-'0'+b[i]-'0'; */
+/* 	result = char((tmp+carry)%10+'0')+result; */
+/* 	carry = tmp/10; */
+/*     } */
+/*     if(carry > 0) */
+/* 	result = char(carry+'0')+result; */
+/*     return result; */
+/* } */
 string sum(const string &a, const string &b) {
     string c;
     int ia = a.size() - 1, ib = b.size() - 1;
@@ -29,6 +46,7 @@ string sum(const string &a, const string &b) {
 }
 
 string divide_2(const string &a) {
+    /* cout << "DIV " << a << '\n'; */
     if(a == "0" || a == "1")
 	return "0";
     string result;
@@ -45,6 +63,7 @@ string divide_2(const string &a) {
 
 
 string product_1(const string &a, char d) {
+    /* cout << "PRO1\n"; */
     if(d == '0')
 	return "0";
     int d_int = d-'0';
@@ -61,6 +80,7 @@ string product_1(const string &a, char d) {
 }
 
 string product_ru(string a, string b) {
+    /* cout << "PROD\n"; */
     if(a == "0")
 	return "0";
     string result = "0";
@@ -74,6 +94,7 @@ string product_ru(string a, string b) {
 }
 
 string ll2str(ll a) {
+    /* cout << "2STR\n"; */
     string result;
     while(a > 0) {
 	char ch = a%10+'0';
@@ -84,6 +105,7 @@ string ll2str(ll a) {
 }
 
 string modulo(string a, ll n) {
+    /* cout << "MOD\n"; */
     ll result = 0;
     for(int i = 0; i < a.size(); i++)
 	result = (result * 10 + a[i]-'0') % n;
@@ -94,6 +116,7 @@ string modulo(string a, ll n) {
 }
 
 string modular_power(string p, string exponent, ll n) {
+    /* cout << "MOD_POW " << exponent << '\n'; */
     if(exponent == "0")
 	return "1";
     string x = modular_power(p, divide_2(exponent), n);
@@ -104,12 +127,24 @@ string modular_power(string p, string exponent, ll n) {
     return x;
 }
 
+void test_functions() {
+    cout << "SUM: " << sum("141241523", "1512553") << '\n'
+	 << "DIV: " << divide_2("412421611") << '\n'
+	 << "PRO1:" << product_1("644132416", '8') << '\n'
+	 << "PRO_R:"<< product_ru("12125414", "13123642") << '\n'
+	 << "PRO_R:"<< product_ru("12", "513561") << '\n'
+	 << "2STR: "<< ll2str(412411251252151) << '\n'
+	 << "MOD: " << modulo("1425315115", 124124) << " (" << 1425315115%124124 << ")\n"
+	 << "MOD_POW"<<modular_power("2","100",10) << '\n';
+}
+
 int main() {
     std::ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     int t;
     string x,y;
     ll n;
+    /* test_functions(); */
     cin >> t;
     for(int i = 0; i < t; i++) {
 	cin >> x >> y >> n;
